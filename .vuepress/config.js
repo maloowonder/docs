49fc8e88b48f71dcbf52fd361fd101f45457f52a
mlimli <maloowonder@126.com>
2020-07-05 19:29:34 +0800

    基本搭建完成

---
+++ .vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: '公考手册',
+  description: '公务员 公考 国考 省考 行测 申论 上岸 知识 培训',
+  head:[
+    ['link',{ rel: 'icon',href:'/avatar.png' }],
+  ],
+    themeConfig: {
+      logo: '/avatar.png',
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '资料下载', link: '/guide/' },
+        { text: '使用反馈', link: 'https://baidu.com' },
+      ],
+      sidebar: 'auto' ,
+      smoothScroll: true
+    }
+  }
